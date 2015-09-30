39176acdb14801d599a038a59978779f688e7a75
Pawel <pawel.kozela@gmail.com>
2015-09-30 18:14:39 +0200

    Added MIN_CHILD_WEIGHT to parameters

--- src/model_evaluation/xgboost_tester.py
+++ src/model_evaluation/xgboost_tester.py
@@ -5,10 +5,11 @@ from datetime import datetime
 
 class XGBoostTester(BaseTester):
 
-    RANGE_MAX_DEPTH = [6, 12, 24]
     RANGE_ETA = [.005, .01, .05, .1]
-    RANGE_COL_SAMPLE = [.05, .1, .25, .75, 1]
-    RANGE_SUB_SAMPLE = [.25, .5, .75, 1]
+    RANGE_MAX_DEPTH = [6, 12]
+    RANGE_COL_SAMPLE = [.05, .1, .25, 1]
+    RANGE_SUB_SAMPLE = [.5, 1]
+    RANGE_MIN_CHILD_WEIGHT = [1, 10, 100]
 
     NB_ROUNDS = 5 * 1000
 
@@ -16,34 +17,36 @@ class XGBoostTester(BaseTester):
         dtrain = xgb.DMatrix(x_train, y_train['target'])
         dtest = xgb.DMatrix(x_validation, y_validation['target'])
 
-        for i_max_depth in self.RANGE_MAX_DEPTH:
-            for i_eta in self.RANGE_ETA:
+        for i_eta in self.RANGE_ETA:
+            for i_max_depth in self.RANGE_MAX_DEPTH:
                 for i_col_sample in self.RANGE_COL_SAMPLE:
                     for i_sub_sample in self.RANGE_SUB_SAMPLE:
-                        evals_result = {}
-                        params = {'bst:max_depth': i_max_depth,
-                                  'bst:eta': i_eta,
-                                  'objective': 'binary:logistic',
-                                  'colsample_bytree': i_col_sample,
-                                  'subsample': i_sub_sample,
-                                  'eval_metric': 'auc',
-                                  'silent': 1,
-                                  'nthread': 4}
+                        for i_min_child_weight in self.RANGE_MIN_CHILD_WEIGHT:
+                            evals_result = {}
+                            params = {'bst:max_depth': i_max_depth,
+                                      'bst:eta': i_eta,
+                                      'objective': 'binary:logistic',
+                                      'colsample_bytree': i_col_sample,
+                                      'subsample': i_sub_sample,
+                                      'min_child_weight': i_min_child_weight,
+                                      'eval_metric': 'auc',
+                                      'silent': 1,
+                                      'nthread': 4}
 
-                        eval_list = [(dtrain, 'train'), (dtest, 'eval')]
-                        bst = xgb.train(params, dtrain, self.NB_ROUNDS, eval_list, evals_result=evals_result, verbose_eval=False, early_stopping_rounds=200)
+                            eval_list = [(dtrain, 'train'), (dtest, 'eval')]
+                            bst = xgb.train(params, dtrain, self.NB_ROUNDS, eval_list, evals_result=evals_result, verbose_eval=False, early_stopping_rounds=200)
 
-                        timestamp = datetime.now()
-                        model_id = 'XGBoost_{:%Y%m%d_%H%M%S}'.format(timestamp)
-                        result = {'model_id': model_id,
-                                  'model_type': 'XGBoost',
-                                  'timestamp': timestamp,
-                                  'features_model': features_model,
-                                  'score': evals_result['eval'][-1],
-                                  'nb_rounds_max': self.NB_ROUNDS,
-                                  'nb_rounds': len(evals_result['eval']),
-                                  'params': params}
+                            timestamp = datetime.now()
+                            model_id = 'XGBoost_{:%Y%m%d_%H%M%S}'.format(timestamp)
+                            result = {'model_id': model_id,
+                                      'model_type': 'XGBoost',
+                                      'timestamp': timestamp,
+                                      'features_model': features_model,
+                                      'score': evals_result['eval'][-1],
+                                      'nb_rounds_max': self.NB_ROUNDS,
+                                      'nb_rounds': len(evals_result['eval']),
+                                      'params': params}
 
-                        print result
+                            print result
 
-                        self.save_result(result)
+                            self.save_result(result)
