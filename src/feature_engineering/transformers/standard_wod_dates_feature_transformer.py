724fb9bed635cc169a938406a0b05caaf6b2906c
Pawel <pawel.kozela@gmail.com>
2015-10-05 00:13:56 +0200

    Added 2 new feature sets: - with date features - with date features / without duplicates

---
+++ src/feature_engineering/transformers/standard_wod_dates_feature_transformer.py
@@ -0,0 +1,62 @@
+from sklearn import preprocessing
+import numpy as np
+
+from base_feature_transformer import BaseFeatureTransformer
+from .. import feature_utilities
+
+NA_VALUE = -1
+
+
+class StandardWodDatesFeatureTransformer(BaseFeatureTransformer):
+
+    def transform_object_features(self):
+        for i in range(0, self._train_features.shape[1]):
+            feature_type = feature_utilities.identify_feature(self._train_features.iloc[:, i].append(self._test_features.iloc[:, i]))
+
+            if feature_type[0] == 'Object' and feature_type[1] == 'Date':
+                self._train_features.iloc[:, i] = self._train_features.iloc[:, i].apply(feature_utilities.transform_date)
+                self._test_features.iloc[:, i] = self._test_features.iloc[:, i].apply(feature_utilities.transform_date)
+
+            elif feature_type[0] == 'Object':
+                le = preprocessing.LabelEncoder()
+                le.fit(self._train_features.iloc[:, i].append(self._test_features.iloc[:, i]))
+
+                self._train_features.iloc[:, i] = le.transform(self._train_features.iloc[:, i])
+                self._test_features.iloc[:, i] = le.transform(self._test_features.iloc[:, i])
+
+    def remove_duplicates(self):
+        to_remove = []
+        for i in range(0, self._train_features.shape[1]):
+            if self._train_features.iloc[:, i].append(self._test_features.iloc[:, i]).value_counts().shape[0] == 1:
+                to_remove.append(self._train_features.columns[i])
+            elif i < self._train_features.shape[1] - 1 and (self._train_features.iloc[:, i] == self._train_features.iloc[:, i+1]).all():
+                to_remove.append(self._train_features.columns[i])
+
+        print to_remove
+        self._train_features.drop(to_remove, inplace=True, axis=1)
+        self._test_features.drop(to_remove, inplace=True, axis=1)
+
+    def add_dates(self):
+        date_features = ['VAR_0073', 'VAR_0075', 'VAR_0176', 'VAR_0179', 'VAR_0204', 'VAR_0217']
+
+        for i in range(0, len(date_features)):
+            for j in range(i+1, len(date_features)):
+                feature_name = '{}_{}'.format(date_features[i], date_features[j])
+                self._train_features[feature_name] = self._train_features[date_features[i]] - self._train_features[date_features[j]]
+                self._test_features[feature_name] = self._test_features[date_features[i]] - self._test_features[date_features[j]]
+
+    def transform_features(self):
+
+        self.transform_object_features()
+
+        self._train_features['MOD_0217'] = np.mod(self._train_features['VAR_0217'], 7)
+        self._train_features['MOD_0075'] = np.mod(self._train_features['VAR_0075'], 7)
+        self._train_features.fillna(NA_VALUE, inplace=True)
+
+        self._test_features['MOD_0217'] = np.mod(self._test_features['VAR_0217'], 7)
+        self._test_features['MOD_0075'] = np.mod(self._test_features['VAR_0075'], 7)
+        self._test_features.fillna(NA_VALUE, inplace=True)
+
+        self.remove_duplicates()
+
+        self.add_dates()
